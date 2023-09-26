e5b4290725c5d0765a0490752248e367e249f7d4
Sylvester <violetsmyster@gmail.com>
2023-09-26 16:21:08 +0100

    Update onboarding.js

--- src/onboarding/onboarding.js
+++ src/onboarding/onboarding.js
@@ -1,57 +1,53 @@
-"use strict";
+'use strict'
 
 /* global chrome */
 
 import * as ch from '../chrome/promisify.js'
 
-window.addEventListener("load", init);
+window.addEventListener('load', init)
 
-async function init() {
+async function init () {
   try {
-    await insertStrings();
+    await insertStrings()
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 
-  fadeInPage();
+  fadeInPage()
 }
 
-async function insertStrings() {
+async function insertStrings () {
   try {
-    const strings = document.querySelectorAll("[data-localize]");
+    const strings = document.querySelectorAll('[data-localize]')
 
     if (strings) {
       for (const s of strings) {
-        s.innerHTML = chrome.i18n.getMessage(s.dataset.localize);
+        s.innerHTML = chrome.i18n.getMessage(s.dataset.localize)
       }
     }
 
-    const accelerators = document.querySelectorAll("[data-accelerator]");
+    const accelerators = document.querySelectorAll('[data-accelerator]')
 
     const platformInfo = await ch.getPlatformInfo().catch((error) => {
-      console.error(error);
-    });
+      console.error(error)
+    })
 
     if (accelerators) {
       for (const a of accelerators) {
-        if (platformInfo.os === "mac") {
-          a.innerText = chrome.i18n.getMessage(`ACCELERATOR_${a.dataset.accelerator}_MAC`);
+        if (platformInfo.os === 'mac') {
+          a.innerText = chrome.i18n.getMessage(`ACCELERATOR_${a.dataset.accelerator}_MAC`)
         } else {
-          a.innerText = chrome.i18n.getMessage(`ACCELERATOR_${a.dataset.accelerator}`);
+          a.innerText = chrome.i18n.getMessage(`ACCELERATOR_${a.dataset.accelerator}`)
         }
       }
     }
 
     document.getElementById('version').innerText = `v${chrome.runtime.getManifest().version}`
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
-function fadeInPage() {
-  document.querySelector(".container").classList.remove("hidden");
-}
-
-function onContinueButtonClicked() {
-  window.close();
+function fadeInPage () {
+  document.querySelector('.container').classList.remove('hidden')
 }
